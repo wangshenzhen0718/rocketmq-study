3ea5ff5cc33642cbc1a601547121baa9b3b505e7
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-05-22 22:23:44 +0800

    生产者发送消息

--- a-rocketmq-demo/src/test/java/com/wang/ARocketmqDemoApplicationTests.java
+++ a-rocketmq-demo/src/test/java/com/wang/ARocketmqDemoApplicationTests.java
@@ -1,5 +1,9 @@
 package com.wang;
 
+import com.wang.constant.MqConstant;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ARocketmqDemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		DefaultMQProducer producer = new DefaultMQProducer("test-producer-group");
+		producer.setNamesrvAddr(MqConstant.NAME_SRV_ADDR);
+		producer.start();
+		Message message = new Message("testTopic","我是一个测试消息".getBytes());
+		SendResult sendResult = producer.send(message);
+		System.out.println(sendResult.getSendStatus());
+		System.out.println(sendResult);
+		producer.shutdown();
+
+
 	}
 
 }
---
+++ a-rocketmq-demo/src/test/java/com/wang/constant/MqConstant.java
@@ -0,0 +1,6 @@
+package com.wang.constant;
+
+public interface MqConstant {
+
+    String NAME_SRV_ADDR = "192.168.1.178:9876";
+}
