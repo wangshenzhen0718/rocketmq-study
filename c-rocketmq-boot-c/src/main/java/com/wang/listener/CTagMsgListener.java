e5bff721e1602041f8bc3937ebc2e2b2edebc7e4
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-05-28 22:37:17 +0800

    format

---
+++ c-rocketmq-boot-c/src/main/java/com/wang/listener/CTagMsgListener.java
@@ -0,0 +1,27 @@
+package com.wang.listener;
+
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.annotation.SelectorType;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: DLJD
+ * @Date: 2023/4/22
+ */
+@Component
+@RocketMQMessageListener(topic = "bootTagTopic",
+        consumerGroup = "boot-tag-consumer-group",
+        selectorType = SelectorType.TAG,// tag过滤模式
+        selectorExpression = "tagA || tagB"
+//        selectorType = SelectorType.SQL92,// sql92过滤模式
+//        selectorExpression = "a in (3,5,7)" // broker.conf中开启enbalePropertyFilter=true
+)
+public class CTagMsgListener implements RocketMQListener<MessageExt> {
+
+    @Override
+    public void onMessage(MessageExt message) {
+        System.out.println(new String(message.getBody()));
+    }
+}
